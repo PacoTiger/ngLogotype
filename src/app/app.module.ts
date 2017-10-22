4cdf0b7fa5451ea691a467e00f1ff852cb88bb2e
Francisco Padilla Muñoz <padilla.fco@gmail.com>
2017-10-23 00:23:36 +0100

    Add Navigate bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
+const appRoutes: Routes = [
+  { path: '/nav', component: NavBarComponent },
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
